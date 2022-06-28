1695d1af17976e70cfff867d8ce8732903199923
Reycho <62747204+Reycho@users.noreply.github.com>
2022-06-28 14:56:25 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,21 @@
+let timesincelaststep = 0
 let step = 0
 radio.setGroup(255)
+let laststeptime = -100
 basic.showLeds(`
     # # # # #
     . . # . .
-    . # # . .
+    . # # # .
     . . # . .
     . . # . .
     `)
 basic.forever(function () {
     radio.sendValue("accel", input.acceleration(Dimension.Strength))
     radio.sendValue("step", step)
-    if (input.acceleration(Dimension.Strength) > 1500) {
-        basic.pause(350)
+    timesincelaststep = control.millis() - laststeptime
+    if (input.acceleration(Dimension.Strength) > 1500 && timesincelaststep > 350) {
         step += 1
+        laststeptime = control.millis()
     }
-    basic.pause(1)
+    basic.pause(30)
 })
